4aa6c03a93413e4b97088e6dc22c46e23c979fd1
Seymour-Lee <894951782@qq.com>
2018-07-15 10:45:40 +0800

    finish leetcode 27

---
+++ leetcode/027. Remove Element.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int removeElement(vector<int>& nums, int val) {
+        int cur = 0;
+        
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[i] != val){
+                nums[cur++] = nums[i];
+            }
+        }
+        
+        return cur;
+    }
+};
