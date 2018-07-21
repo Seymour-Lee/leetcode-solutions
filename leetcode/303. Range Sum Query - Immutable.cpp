13c081db58ec79b891134ec799fe49a9d551f4de
Seymour-Lee <894951782@qq.com>
2018-07-21 12:03:43 +0800

    ac leetcode 303

---
+++ leetcode/303. Range Sum Query - Immutable.cpp
@@ -0,0 +1,77 @@
+class NumArray {
+public:
+    NumArray(vector<int> nums) {
+        sums.push_back(0);
+        for(int i = 0; i < nums.size(); i++){
+            sums.push_back(sums.back() + nums[i]);
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return sums[j+1] - sums[i];
+    }
+    
+private:
+    vector<int> sums;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+
+// MLE
+class NumArray {
+public:
+    NumArray(vector<int> nums) {
+        for(int i = 0; i < nums.size(); i++){
+            vector<int> cur = {nums[i]};
+            sums.push_back(cur);
+        }
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = 1; j < nums.size()-i; j++){
+                sums[i].push_back(sums[i][j-1] + sums[i+j][0]);
+            }
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return sums[i][j-i];
+    }
+    
+private:
+    vector<vector<int>> sums;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
+
+//MLE
+class NumArray {
+public:
+    NumArray(vector<int> nums) {
+        for(int i = 0; i < nums.size(); i++) sums[i][i] = nums[i];
+        for(int i = 0; i < nums.size(); i++){
+            for(int j = i + 1; j < nums.size(); j++){
+                sums[i][j] = sums[i][j-1] + sums[j][j];
+            }
+        }
+    }
+    
+    int sumRange(int i, int j) {
+        return sums[i][j];
+    }
+    
+private:
+    map<int, map<int, int>> sums;
+};
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * NumArray obj = new NumArray(nums);
+ * int param_1 = obj.sumRange(i,j);
+ */
