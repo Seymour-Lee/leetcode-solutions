3dc1fac0aaf2133387ad944462d8a39405e95669
Seymour-Lee <894951782@qq.com>
2018-07-27 17:32:33 +0800

    ac leetcode 704 707

---
+++ leetcode/704. Binary Search.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        int l = 0;
+        int r = nums.size()-1;
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(nums[m] == target) return m;
+            if(nums[m] > target) r = m -1;
+            else l = m + 1;
+        }
+        return -1;
+    }
+};
