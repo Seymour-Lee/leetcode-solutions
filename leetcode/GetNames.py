16fc5643ecfa1fc2eb54edf7ddbec0a400125dd4
Seymour-Lee <894951782@qq.com>
2018-09-25 13:22:49 -0700

    add py script to get all ac-ed problems

---
+++ leetcode/GetNames.py
@@ -0,0 +1,15 @@
+import os
+
+dir_path = '.'
+outf = open('list.txt', 'w')
+
+strs = os.listdir(dir_path)
+
+
+
+for str in sorted(strs):
+    if str.find(".cpp") != -1:
+        name = str[:-4]
+        outf.write(name + "\n")
+
+outf.close()
