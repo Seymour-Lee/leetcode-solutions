c47c1ed5d398cf31008a1497a76c072cf25fd977
Seymour-Lee <894951782@qq.com>
2018-10-16 09:48:59 -0700

    add new method for leetcode 21

--- leetcode/021. Merge Two Sorted Lists.cpp
+++ leetcode/021. Merge Two Sorted Lists.cpp
@@ -44,3 +44,26 @@ public:
         return dummy->next;
     }
 };
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode *dummy = new ListNode(-1);
+        ListNode *cur = dummy;
+        while(l1 && l2){
+            if(l1->val <= l2->val) cur->next = l1, cur = cur->next, l1 = l1->next;
+            else cur->next = l2, cur = cur->next, l2 = l2->next;
+        }
+        if(l1) cur->next = l1;
+        if(l2) cur->next = l2;
+        return dummy->next;
+    }
+};
