7ca23e5fdac914cdf5c8b5dc3497b6348c6d3d43
Seymour-Lee <894951782@qq.com>
2018-10-27 17:56:10 -0700

    add new codes

--- leetcode/033. Search in Rotated Sorted Array.cpp
+++ leetcode/033. Search in Rotated Sorted Array.cpp
@@ -22,3 +22,28 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        if(nums.size() == 0) return -1;
+        int l = 0, r = nums.size()-1;
+        while(l <= r){
+            int m = l + (r - l) / 2;
+            if(nums[m] == target) return m;
+            if(nums[m] < nums[l]){
+                if(nums[m] < target && target <= nums[r]) l = m+1;
+                else r = m-1;
+            }
+            else if(nums[m] > nums[r]){
+                if(nums[l] <= target && target < nums[m]) r = m-1;
+                else l = m+1;
+            }
+            else{
+                if(target < nums[m]) r = m-1;
+                else l = m+1;
+            }
+        }
+        return -1;
+    }
+};
