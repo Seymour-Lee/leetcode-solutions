4ed1920356067e0ac89b01539627c26ba0fd9483
Seymour-Lee <894951782@qq.com>
2018-11-14 09:47:13 -0800

    add codes

--- leetcode/241. Different Ways to Add Parentheses.cpp
+++ leetcode/241. Different Ways to Add Parentheses.cpp
@@ -29,3 +29,33 @@ private:
         {'-', [](int a, int b){return a-b;}},
         {'*', [](int a, int b){return a*b;}}};
 };
+
+class Solution {
+public:
+    vector<int> diffWaysToCompute(string input) {
+        vector<int> ans;
+        if(input == "") return ans;
+        if(input.find('+') == string::npos &&
+           input.find('-') == string::npos &&
+           input.find('*') == string::npos){
+            ans.push_back(stoi(input));
+        }
+        for(int i = 0; i < input.size(); i++){
+            if(input[i] == '+' || input[i] == '-' || input[i] == '*'){
+                auto left = diffWaysToCompute(input.substr(0, i));
+                auto right = diffWaysToCompute(input.substr(i+1));
+                for(auto l: left) for(auto r: right) ans.push_back(op2func[input[i]](l, r));
+            }
+        }
+        return ans;
+    }
+    
+private:
+    unordered_map<char, int(*)(int, int)> op2func = {
+        {'+', [](int a, int b){return a+b;}},
+        {'-', [](int a, int b){return a-b;}},
+        {'*', [](int a, int b){return a*b;}},
+    };
+    
+    
+};
