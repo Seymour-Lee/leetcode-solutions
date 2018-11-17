166fa78448c319ef16b6fb6f1cffe4d4b19c7e61
Seymour-Lee <894951782@qq.com>
2018-11-17 14:48:39 -0800

    add

--- leetcode/940. Distinct Subsequences II.cpp
+++ leetcode/940. Distinct Subsequences II.cpp
@@ -14,3 +14,21 @@ public:
         return (int)(dp[n]-1);
     }
 }; 
+
+class Solution {
+public:
+    int distinctSubseqII(string S) {
+        const long long mod = 1000000000+7;
+        int n = S.size();
+        vector<long long> last(26, -1);
+        vector<long long> dp(n+1, 0);
+        dp[0] = 1;
+        for(int i = 1; i <= n ;i++){
+            char ch = S[i-1]-'a';
+            dp[i] = (2*dp[i-1]) % mod;
+            if(last[ch] != -1) dp[i] = (dp[i] - dp[last[ch]] + mod) % mod;
+            last[ch] = i-1;
+        }
+        return (int)(dp[n]-1);
+    }
+}; 
