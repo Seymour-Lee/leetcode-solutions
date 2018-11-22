5a158f4bd57337ee17e558fd821612bd4bb42fa4
Seymour-Lee <894951782@qq.com>
2018-11-22 11:43:36 -0800

    add

--- leetcode/137. Single Number II.cpp
+++ leetcode/137. Single Number II.cpp
@@ -25,3 +25,15 @@ public:
         return a;
     }
 };
+
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int ones = 0, twos = 0;
+        for(auto num: nums){
+            ones = (ones ^ num) & (~twos);
+            twos = (twos ^ num) & (~ones);
+        }
+        return ones;
+    }
+};
