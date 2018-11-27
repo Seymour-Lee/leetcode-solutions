30eae1d51f814ec765d6e09bb2c01ed5d71e0208
Seymour-Lee <894951782@qq.com>
2018-11-26 23:51:16 -0800

    ac leetcode 123

--- leetcode/122. Best Time to Buy and Sell Stock II.cpp
+++ leetcode/122. Best Time to Buy and Sell Stock II.cpp
@@ -8,3 +8,14 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        if(prices.size() == 0) return 0;
+        int ans = 0;
+        for(int i = 1; i < prices.size(); i++) 
+            ans += prices[i]>prices[i-1]? prices[i]-prices[i-1]:0;
+        return ans;
+    }
+};
