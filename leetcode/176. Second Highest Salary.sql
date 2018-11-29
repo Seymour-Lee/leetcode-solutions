50658e84ee0c31769ebe4da6517c60173df5b1f1
Seymour-Lee <894951782@qq.com>
2018-11-29 10:33:28 -0800

    ac leetcode 175, 176, 181, 182, 183

---
+++ leetcode/176. Second Highest Salary.sql
@@ -0,0 +1,8 @@
+# Write your MySQL query statement below
+select(
+    select distinct Salary
+    from Employee
+    order by Salary desc
+    limit 1 offset 1
+) as SecondHighestSalary
+
---
+++ leetcode/183. Customers Who Never Order.sql
@@ -0,0 +1,4 @@
+# Write your MySQL query statement below
+select customers.name as 'Customers'
+from customers
+where customers.id not in (select customerid from orders)
