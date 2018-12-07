266f868ea865b6ff3872637ea19da94d62f674b0
Seymour-Lee <894951782@qq.com>
2018-12-07 09:19:02 -0800

    add codes

--- leetcode/540. Single Element in a Sorted Array.cpp
+++ leetcode/540. Single Element in a Sorted Array.cpp
@@ -25,3 +25,23 @@ public:
         return nums[l];
     }
 };
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l = 0, r = nums.size()-1;
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if(m % 2 == 0) {
+                if(nums[m] == nums[m+1]) l = m + 2;
+                else if(nums[m] == nums[m-1]) r = m - 2;
+                else return nums[m];
+            }
+            else{
+                if(nums[m] == nums[m+1]) r = m - 1;
+                else l = m + 1;
+            }
+        }
+        return nums[l];
+    }
+};
