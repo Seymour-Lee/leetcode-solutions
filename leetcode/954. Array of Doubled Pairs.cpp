20f91e540c17097b411942ece012244e53b76a32
Seymour-Lee <894951782@qq.com>
2018-12-08 23:31:42 -0800

    ac leetcode 953, 954

---
+++ leetcode/954. Array of Doubled Pairs.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool canSortDoubled(vector<int>& A) {
+        multiset<int> nums(A.begin(), A.end());
+        while(nums.empty() == false){
+            auto b = nums.begin();
+            auto lowest = *b;
+            nums.erase(b);
+            if(lowest < 0){
+                if(lowest % 2 == 1) return false;
+                auto itor = nums.find(lowest/2);
+                if(itor == nums.end()) return false;
+                nums.erase(itor);
+            }
+            else{
+                auto itor = nums.find(2*lowest);
+                if(itor == nums.end()) return false;
+                nums.erase(itor);
+            }
+        }
+        return true;
+    }
+};
