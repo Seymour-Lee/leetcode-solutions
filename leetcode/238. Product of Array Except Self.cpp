90eeaefe5370f6db5bd66ce3f2f436eef470d6b3
Seymour-Lee <894951782@qq.com>
2018-12-24 11:07:27 -0800

    add codes

--- leetcode/238. Product of Array Except Self.cpp
+++ leetcode/238. Product of Array Except Self.cpp
@@ -15,3 +15,21 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums) {
+        vector<int> ans(nums.size(), 1);
+        int l = 1;
+        for(int i = 0; i < nums.size(); i++){
+            ans[i] = ans[i] * l;
+            l = l * nums[i];
+        }
+        int r = 1;
+        for(int i = nums.size()-1; i >= 0; i--){
+            ans[i] = ans[i] * r;
+            r = r * nums[i];
+        }
+        return ans;
+    }
+};
