ff46e932113d209c80cfc0311f8dd43fc6d46a18
Seymour-Lee <894951782@qq.com>
2019-01-12 17:16:27 -0800

    add codes

--- leetcode/076. Minimum Window Substring.cpp
+++ leetcode/076. Minimum Window Substring.cpp
@@ -19,3 +19,28 @@ public:
         return len == INT_MAX? "": s.substr(pos, len);
     }
 };
+
+class Solution {
+public:
+    string minWindow(string s, string t) {
+        if(s.size() == 0 || t.size() == 0) return "";
+        int pos = 0;
+        int len = INT_MAX;
+        int i = 0, j = 0;
+        unordered_map<char, int> ch2num;
+        int cnt = t.size();
+        for(auto c: t) ch2num[c]++;
+        while(i <= j && j < s.size()){
+            char a = s[j++];
+            if(ch2num[a] > 0) cnt--;
+            ch2num[a]--;
+            while(cnt == 0){
+                if(j-i < len) len = j-i, pos = i;
+                char b = s[i++];
+                if(ch2num[b] == 0) cnt++;
+                ch2num[b]++;
+            }
+        }
+        return len == INT_MAX? "": s.substr(pos, len);
+    }
+};
