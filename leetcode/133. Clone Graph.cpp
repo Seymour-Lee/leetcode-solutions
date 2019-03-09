3543456f1b8d56145417275691dc52fa9d41a437
Seymour-Lee <894951782@qq.com>
2019-03-09 09:07:35 -0800

    add

--- leetcode/133. Clone Graph.cpp
+++ leetcode/133. Clone Graph.cpp
@@ -28,3 +28,40 @@ private:
         return cur;
     }
 };
+
+/*
+// Definition for a Node.
+class Node {
+public:
+    int val;
+    vector<Node*> neighbors;
+
+    Node() {}
+
+    Node(int _val, vector<Node*> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+};
+*/
+class Solution {
+public:    
+    Node* cloneGraph(Node* node) {
+        if(node == nullptr) return nullptr;
+        map<int, Node *> label2node;
+        return dfs(node, label2node);
+    }
+
+private:
+    Node *dfs(Node *node, map<int, Node *> &label2node){
+        Node *cur = label2node[node->val];
+        if(cur == nullptr){
+            cur = new Node(node->val);
+            label2node[node->val] = cur;
+            for(auto n: node->neighbors){
+                (cur->neighbors).push_back(dfs(n, label2node));
+            }
+        }
+        return cur;
+    }
+};
