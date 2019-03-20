ed8894517ddf243b4ec4a951bc76c709141cacc5
Seymour-Lee <894951782@qq.com>
2019-03-20 10:56:08 -0700

    add

--- leetcode/501. Find Mode in Binary Search Tree.cpp
+++ leetcode/501. Find Mode in Binary Search Tree.cpp
@@ -30,3 +30,46 @@ private:
         dfs(root->right, table);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    vector<int> findMode(TreeNode* root) {
+        vector<int> ans;
+        dfs(root, ans);
+        return ans;
+    }
+    
+private:
+    int modecounter = 0;
+    int curcounter = 0;
+    int curval = INT_MIN;
+    
+    void dfs(TreeNode *root, vector<int> &ans){
+        if(root == nullptr) return;
+        dfs(root->left, ans);
+        if(root->val != curval){
+            curval = root->val;
+            curcounter = 0;
+        }
+        curcounter++;
+        if(curcounter > modecounter){
+            ans = {};
+            ans.push_back(root->val);
+            modecounter = curcounter;
+        }
+        else if(curcounter == modecounter){
+            ans.push_back(root->val);
+            // modecounter++;
+        }
+        dfs(root->right, ans);
+    }
+};
