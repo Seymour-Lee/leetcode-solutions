fcb85608875679d53fd71041613746f178015d2e
Seymour-Lee <894951782@qq.com>
2019-04-05 11:22:13 -0700

    add

--- leetcode/593. Valid Square.cpp
+++ leetcode/593. Valid Square.cpp
@@ -19,3 +19,14 @@ public:
                && dis[4] == dis[5] && dis[0] != 0 && dis[4] != 0;
     }
 };
+
+class Solution {
+public:
+    bool validSquare(vector<int>& p1, vector<int>& p2, vector<int>& p3, vector<int>& p4) {
+        auto f = [](vector<int> &a, vector<int> &b){
+            return (a[0]-b[0])*(a[0]-b[0])+(a[1]-b[1])*(a[1]-b[1]);
+        };
+        unordered_set<int> dis = {f(p1, p2), f(p2, p3), f(p3, p4), f(p4, p1), f(p1, p3), f(p2, p4)};
+        return dis.size() == 2 && dis.find(0) == dis.end();
+    }
+};
