3c3df629c8aa0621a676ac2a91d148b826d656a1
Seymour-Lee <yli792@usc.edu>
2019-04-26 18:57:07 -0700

    add codes for 031

--- leetcode/031. Next Permutation.cpp
+++ leetcode/031. Next Permutation.cpp
@@ -23,3 +23,27 @@ public:
         swap(nums[prev], nums[next]);
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        vector<vector<int>> ans;
+        sort(nums.begin(), nums.end());
+        ans.push_back(nums);
+        while([](vector<int> &nums) -> bool{
+            if(nums.size() <= 1) return false;
+            int prev = nums.size()-2;
+            while(prev >= 0 && nums[prev] >= nums[prev+1]) prev--;
+            if(prev == -1){
+                reverse(nums.begin(), nums.end());
+                return false;
+            }
+            reverse(nums.begin()+prev+1, nums.end());
+            int next = prev+1;
+            while(nums[prev] >= nums[next]) next++;
+            swap(nums[prev], nums[next]);
+            return true;
+        }(nums)) ans.push_back(nums);
+        return ans;
+    }
+};
