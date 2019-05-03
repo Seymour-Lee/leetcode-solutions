9d3c620ec62d78b7978fb17a4f2e386a4840fb69
Seymour-Lee <yli792@usc.edu>
2019-05-02 20:56:20 -0700

    add

--- leetcode/115. Distinct Subsequences.cpp
+++ leetcode/115. Distinct Subsequences.cpp
@@ -27,3 +27,18 @@ public:
         return dp.back().back();
     }
 };
+
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+        long long m = s.size(), n = t.size();
+        vector<vector<long long>> dp(m+1, vector<long long>(n+1, 0));
+        for(long long i = 0; i < dp.size(); i++) dp[i][0] = 1;
+        for(long long j = 1; j <= n; j++){
+            for(long long i = j; i <= m; i++){
+                dp[i][j] = dp[i-1][j] + (s[i-1] == t[j-1]? dp[i-1][j-1]: 0);
+            }
+        }
+        return dp.back().back();
+    }
+};
