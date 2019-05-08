af2da34c5c94332cbb8ab1c957ad9f99b0b2315c
Seymour-Lee <yli792@usc.edu>
2019-05-08 22:03:27 +0800

    add

--- leetcode/312. Burst Balloons.cpp
+++ leetcode/312. Burst Balloons.cpp
@@ -17,3 +17,23 @@ public:
         return dp[0].back();
     }
 };
+
+class Solution {
+public:
+    int maxCoins(vector<int>& nums) {
+        nums.insert(nums.begin(), 1);
+        nums.push_back(1);
+        vector<vector<int>> dp(nums.size(), vector<int>(nums.size(), 0));
+        for(int k = 2; k < nums.size(); k++){
+            for(int left = 0; left < nums.size()-k; left++){
+                int right = left + k;
+                for(int i = left+1; i < right; i++){
+                    dp[left][right] = max(dp[left][right],
+                        nums[left]*nums[i]*nums[right]+dp[left][i]+dp[i][right]);
+                }
+            }
+        }
+        
+        return dp[0].back();
+    }
+};
