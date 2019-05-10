6e95d1b93b36023494d568de9382bad13f2e0fd0
Seymour-Lee <yli792@usc.edu>
2019-05-10 17:57:17 +0800

    add

--- leetcode/1032. Stream of Characters.cpp
+++ leetcode/1032. Stream of Characters.cpp
@@ -53,3 +53,58 @@ private:
  * StreamChecker* obj = new StreamChecker(words);
  * bool param_1 = obj->query(letter);
  */
+
+\class Trie{
+public:
+    Trie(): children(vector<Trie*>(26, nullptr)), end(false){}
+    
+    void insert(string &str){
+        reverse(str.begin(), str.end());
+        auto node = this;
+        for(auto c: str){
+            int i = c - 'a';
+            if(node->children[i] == nullptr) node->children[i] = new Trie();
+            node = node->children[i];
+        }
+        node->end = true;
+    }
+    
+    bool search(string &str){
+        auto node = this;
+        for(auto c: str){
+            int i = c - 'a';
+            if(node->children[i] == nullptr) return false;
+            node = node->children[i];
+            if(node->end) return true;
+        }
+        return node->end;
+    }
+    
+private:
+    vector<Trie *> children;
+    bool end;
+};
+
+class StreamChecker {
+public:
+    StreamChecker(vector<string>& words) {
+        for(auto &w: words) trie->insert(w), maxlen = max(maxlen, (int)w.size());
+    }
+    
+    bool query(char letter) {
+        str = letter + str;
+        if(str.size() > maxlen) str.pop_back();
+        return trie->search(str);
+    }
+
+private:
+    string str;
+    int maxlen = 0;
+    Trie *trie = new Trie();
+};
+
+/**
+ * Your StreamChecker object will be instantiated and called as such:
+ * StreamChecker* obj = new StreamChecker(words);
+ * bool param_1 = obj->query(letter);
+ */
