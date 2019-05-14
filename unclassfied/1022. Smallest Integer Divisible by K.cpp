1b15133ffe018184b7a1caf4c33e24e3f05a8779
Seymour-Lee <yli792@usc.edu>
2019-05-14 16:11:40 +0800

    add

--- unclassfied/1022. Smallest Integer Divisible by K.cpp
+++ unclassfied/1022. Smallest Integer Divisible by K.cpp
@@ -14,3 +14,30 @@ public:
         return -1;
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int sumRootToLeaf(TreeNode* root) {
+        int ans = 0;
+        dfs(root, ans, 0);
+        return ans;
+    }
+    
+private:
+    void dfs(TreeNode *root, int &ans, int cur){
+        if(root == nullptr) return;
+        cur = cur*2 + root->val;
+        if(root->left == nullptr && root->right == nullptr) ans += cur;
+        dfs(root->left, ans, cur);
+        dfs(root->right, ans, cur);
+    }
+};
