15429fcba48b20d023ea5210f11c81c61bbad7f9
Seymour-Lee <yli792@usc.edu>
2019-06-03 19:07:36 -0700

    add

--- string/028. Implement strStr().cpp
+++ string/028. Implement strStr().cpp
@@ -60,3 +60,11 @@ private:
         return lps;
     }
 };
+
+class Solution {
+public:
+    int strStr(string haystack, string needle) {
+        auto pos = haystack.find(needle);
+        return pos == string::npos? -1: pos;
+    }
+};
