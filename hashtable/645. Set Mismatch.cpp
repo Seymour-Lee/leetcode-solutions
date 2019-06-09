94dc1e4ca7667038bfec51ad5969382b5d328cbc
Seymour-Lee <yli792@usc.edu>
2019-06-09 02:09:11 -0700

    add

---
+++ hashtable/645. Set Mismatch.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int first, second;
+        vector<bool> vec(nums.size()+1, false);
+        for(int i = 0; i < nums.size(); i++){
+            if(vec[nums[i]]){
+                first = nums[i];
+            }
+            vec[nums[i]] = true;
+        }
+        for(int i = 1; i < vec.size(); i++){
+            if(vec[i] == false){
+                second = i;
+                break;
+            }
+        }
+        return vector<int>{first, second};
+    }
+};
+
+class Solution {
+public:
+    vector<int> findErrorNums(vector<int>& nums) {
+        int first, second;
+        vector<bool> vec(nums.size()+1, false);
+        for(int i = 0; i < nums.size(); i++){
+            if(vec[nums[i]]){
+                first = nums[i];
+            }
+            vec[nums[i]] = true;
+        }
+        for(int i = 1; i < vec.size(); i++){
+            if(vec[i] == false){
+                second = i;
+                break;
+            }
+        }
+        return vector<int>{first, second};
+    }
+};
