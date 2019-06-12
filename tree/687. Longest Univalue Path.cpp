614eaa99ac74b8de26c98dc4a6bf45dd805622b3
Seymour-Lee <yli792@usc.edu>
2019-06-11 22:55:38 -0700

    add

--- tree/687. Longest Univalue Path.cpp
+++ tree/687. Longest Univalue Path.cpp
@@ -31,3 +31,36 @@ private:
         return max(curLeft, curRight);
     }
 };
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    int longestUnivaluePath(TreeNode* root) {
+        int ans = 0;
+        dfs(root, ans);
+        return ans;
+    }
+    
+private:
+    // <nodeval, len>
+    pair<int, int> dfs(TreeNode *root, int &ans){
+        if(root == nullptr) return {0, 0};
+        auto l = dfs(root->left, ans);
+        auto r = dfs(root->right, ans);
+        int len = 1;
+        int cnt = 1;
+        if(root->val == l.first) len += l.second, cnt = max(cnt, 1+l.second);
+        if(root->val == r.first) len += r.second, cnt = max(cnt, 1+r.second);
+        ans = max(ans, len-1);
+        // cout<<root->val<<" "<<len<<endl;
+        return {root->val, cnt};
+    }
+};
