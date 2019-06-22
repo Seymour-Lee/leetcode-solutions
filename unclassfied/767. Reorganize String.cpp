522f5586705c637ef617f7b6714985732485c798
Seymour-Lee <yli792@usc.edu>
2019-06-22 16:30:21 -0700

    add

--- unclassfied/767. Reorganize String.cpp
+++ unclassfied/767. Reorganize String.cpp
@@ -20,3 +20,30 @@ public:
         return result;
     }
 };
+
+
+class Solution {
+public:
+    string reorganizeString(string S) {
+        vector<int> c2t(26, 0);
+        for(auto c: S) if((++c2t[c-'a']) > (S.size()+1)/2) return "";
+        using ii = pair<char, int>;
+        auto f = [](ii a, ii b) {return a.second < b.second;};
+        priority_queue<ii, vector<ii>, function<bool(ii, ii)>> q(f);
+        for(int i = 0; i < c2t.size(); i++) if(c2t[i]) q.push({'a'+i, c2t[i]});
+        string ans = "";
+        while(q.empty() == false){
+            auto p = q.top(); q.pop();
+            if(ans.size() && ans.back() == p.first){
+                if(q.empty()) return "";
+                auto pp = q.top(); q.pop();
+                swap(p, pp);
+                q.push(pp);
+            }
+            ans += p.first;
+            p.second--;
+            if(p.second) q.push(p);
+        }
+        return ans;
+    }
+};
