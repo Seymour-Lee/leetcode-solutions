7bce5ad9391ffbfb2f828bd4297d63c0c28a8a79
Seymour-Lee <yli792@usc.edu>
2019-06-30 22:03:07 -0700

    add

--- greedy/452. Minimum Number of Arrows to Burst Balloons.cpp
+++ greedy/452. Minimum Number of Arrows to Burst Balloons.cpp
@@ -26,3 +26,25 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int findMinArrowShots(vector<vector<int>>& ps) {
+        int ans = 0;
+        auto f = [](vector<int> a, vector<int> b){
+            if(a[1] != b[1]) return a[1] < b[1];
+            return a[0] < b[0];
+        };
+        sort(ps.begin(), ps.end(), f);
+        while(ps.empty() == false){
+            auto p = ps.back(); ps.pop_back();
+            int s = p[0];
+            while(ps.empty() == false && ps.back()[1] >= s) {
+                s = max(s, ps.back()[0]);
+                ps.pop_back();
+            }
+            ans++;
+        }
+        return ans;
+    }
+};
