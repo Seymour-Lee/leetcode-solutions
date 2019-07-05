2098c41caf79294f02806ef2b9e7aae2779c52e6
Seymour-Lee <yli792@usc.edu>
2019-07-05 01:37:44 -0700

    add

--- unclassfied/939. Minimum Area Rectangle.cpp
+++ unclassfied/939. Minimum Area Rectangle.cpp
@@ -25,3 +25,22 @@ public:
         return ans == INT_MAX? 0: ans;
     }
 };
+
+class Solution {
+public:
+    int minAreaRect(vector<vector<int>>& points) {
+        int res = INT_MAX;
+        unordered_map<int, set<int>> x;
+         for (auto p : points) x[p[0]].insert(p[1]);
+         for (auto i = x.begin(); i != x.end(); ++i)
+            for (auto j = next(i); j != x.end(); ++j) {
+                if (i->second.size() < 2 || j->second.size() < 2) continue;
+                vector<int> y;
+                set_intersection(begin(i->second), end(i->second),
+                begin(j->second), end(j->second), back_inserter(y));
+                for (int k = 1; k < y.size(); ++k)
+                    res = min(res, abs(j->first - i->first) * (y[k] - y[k - 1]));
+            }
+         return res == INT_MAX ? 0 : res;
+    }
+};
