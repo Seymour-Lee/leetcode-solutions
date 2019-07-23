dd7d3f028aa64dd938f30e01f032697f5aef71bf
Seymour-Lee <yli792@usc.edu>
2019-07-22 23:17:05 -0700

    add

--- unclassfied/456. 132 Pattern.cpp
+++ unclassfied/456. 132 Pattern.cpp
@@ -14,3 +14,20 @@ public:
         return false;
     }
 };
+
+class Solution {
+public:
+    bool find132pattern(vector<int>& nums) {
+        if(nums.size() < 3) return false;
+        int mini = INT_MAX;
+        for(int j = 0; j < nums.size()-1; j++){
+            mini = min(mini, nums[j]);
+            for(int k = j+1; k < nums.size(); k++){
+                if(nums[j] > nums[k] && nums[k] > mini){
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+};
