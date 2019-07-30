d97a6706adfd7d6e058cb4804f332cda0f513ae0
Seymour-Lee <yli792@usc.edu>
2019-07-29 20:44:32 -0700

    add

--- unclassfied/556. Next Greater Element III.cpp
+++ unclassfied/556. Next Greater Element III.cpp
@@ -5,3 +5,11 @@ public:
         return next_permutation(num.begin(), num.end())? stoll(num) > INT_MAX? -1: stoi(num): -1;
     }
 };
+
+class Solution {
+public:
+    int nextGreaterElement(int n) {
+        string num = to_string(n);
+        return next_permutation(num.begin(), num.end())? stoll(num) > INT_MAX? -1: stoi(num): -1;
+    }
+};
