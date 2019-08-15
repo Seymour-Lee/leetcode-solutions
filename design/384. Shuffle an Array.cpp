f6db4f97578b9683e781a747fb359f6afd249a36
Seymour-Lee <yli792@usc.edu>
2019-08-15 01:14:33 -0700

    add

--- design/384. Shuffle an Array.cpp
+++ design/384. Shuffle an Array.cpp
@@ -32,3 +32,35 @@ private:
  * vector<int> param_1 = obj.reset();
  * vector<int> param_2 = obj.shuffle();
  */
+
+class Solution {
+public:
+    Solution(vector<int>& nums) {
+        this->nums = nums;
+    }
+    
+    /** Resets the array to its original configuration and return it. */
+    vector<int> reset() {
+        return nums;
+    }
+    
+    /** Returns a random shuffling of the array. */
+    vector<int> shuffle() {
+        auto r = [](int l, int r){
+            return l + rand()%(r-l);
+        };
+        vector<int> ans = nums;
+        for(int i = 0; i < ans.size(); i++) swap(ans[i], ans[r(i, ans.size())]);
+        return ans;
+    }
+    
+private:
+    vector<int> nums;
+};
+
+/**
+ * Your Solution object will be instantiated and called as such:
+ * Solution* obj = new Solution(nums);
+ * vector<int> param_1 = obj->reset();
+ * vector<int> param_2 = obj->shuffle();
+ */
