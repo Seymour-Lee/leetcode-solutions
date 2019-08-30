da58389d939ee93b66df472b6f69f415ab782105
Seymour-Lee <yli792@usc.edu>
2019-08-29 20:50:28 -0700

    add

--- uf/130. Surrounded Regions.cpp
+++ uf/130. Surrounded Regions.cpp
@@ -71,3 +71,37 @@ private:
         if(xp != yp) p[xp] = yp;
     }
 };
+
+class Solution {
+public:
+    void solve(vector<vector<char>>& board) {
+        if(board.size() == 0 || board[0].size() == 0) return;
+        int m = board.size(), n = board[0].size();
+        vector<int> p(m*n+1, -2);
+        p[m*n] = -1;
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++){
+            if(board[i][j] == 'O'){
+                p[i*n+j] = -1;
+                if(i == 0 || j == 0 || i == m-1 || j == n-1) u(p, i*n+j, m*n);
+                if(i-1 >= 0 && board[i-1][j] == 'O') u(p, i*n+j, (i-1)*n+j);
+                if(j-1 >= 0 && board[i][j-1] == 'O') u(p, i*n+j, i*n+j-1);
+            }
+        }
+        int bp = f(p, m*n);
+        for(int i = 0; i < m; i++) for(int j = 0; j < n; j++){
+            if(board[i][j] == 'O' && f(p, i*n+j) != bp) board[i][j] = 'X';
+        }
+    }
+    
+private:
+    int f(vector<int> &p, int x){
+        if(p[x] == -1) return x;
+        return f(p, p[x]);
+    }
+    
+    void u(vector<int> &p, int x, int y){
+        int xp = f(p, x);
+        int yp = f(p, y);
+        if(xp != yp) p[xp] = yp;
+    }
+};
