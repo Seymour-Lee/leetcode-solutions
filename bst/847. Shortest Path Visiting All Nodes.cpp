af0f1358327cd4f5a8e5e665dee294d9a8e5c61e
Seymour-Lee <yli792@usc.edu>
2019-09-09 17:56:07 -0700

    add

--- bst/847. Shortest Path Visiting All Nodes.cpp
+++ bst/847. Shortest Path Visiting All Nodes.cpp
@@ -24,3 +24,30 @@ public:
         return -1;
     }
 };
+
+class Solution {
+public:
+    int shortestPathLength(vector<vector<int>>& g) {
+        int n = g.size();
+        int target = (1<<n)-1;
+        vector<vector<bool>> v(n, vector<bool>((1<<n), false));
+        queue<pair<int, int>> q;
+        for(int i = 0; i < n; i++) q.push({i, 1<<i}), v[i][1<<i] = true;
+        int ans = 0;
+        while(q.empty() == false){
+            int size = q.size();
+            while(size--){
+                auto p = q.front(); q.pop();
+                int node = p.first;
+                int state = p.second;
+                if(state == target) return ans;
+                for(auto nei: g[node]) if(v[nei][state | (1 << nei)] == false){
+                    v[nei][state | (1 << nei)] = true;
+                    q.push({nei, state|(1<<nei)});
+                }
+            }
+            ans++;
+        }
+        return -1;
+    }
+};
