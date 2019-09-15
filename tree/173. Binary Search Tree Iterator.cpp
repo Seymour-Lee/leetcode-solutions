f616bc94a48ec7c90239b420ece15bb054fd464e
Seymour-Lee <yli792@usc.edu>
2019-09-15 12:57:19 -0700

    add

--- tree/173. Binary Search Tree Iterator.cpp
+++ tree/173. Binary Search Tree Iterator.cpp
@@ -134,3 +134,43 @@ private:
  * int param_1 = obj->next();
  * bool param_2 = obj->hasNext();
  */
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class BSTIterator {
+public:
+    BSTIterator(TreeNode* root) {
+        while(root) stk.push(root), root = root->left;
+    }
+    
+    /** @return the next smallest number */
+    int next() {
+        auto node = stk.top(); stk.pop();
+        int ans = node->val;
+        node = node->right;
+        while(node) stk.push(node), node = node->left;
+        return ans;
+    }
+    
+    /** @return whether we have a next smallest number */
+    bool hasNext() {
+        return stk.size();
+    }
+    
+private:
+    stack<TreeNode *> stk;
+};
+
+/**
+ * Your BSTIterator object will be instantiated and called as such:
+ * BSTIterator* obj = new BSTIterator(root);
+ * int param_1 = obj->next();
+ * bool param_2 = obj->hasNext();
+ */
