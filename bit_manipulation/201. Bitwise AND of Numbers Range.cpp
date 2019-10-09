f47d117678d5f9f4ef415b937d7d1277218115dc
Seymour-Lee <yli792@usc.edu>
2019-10-08 23:38:56 -0700

    add

--- bit_manipulation/201. Bitwise AND of Numbers Range.cpp
+++ bit_manipulation/201. Bitwise AND of Numbers Range.cpp
@@ -14,3 +14,19 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int rangeBitwiseAnd(int m, int n) {
+        if((long)m * (long)n == 0) return 0;
+        if(m == n) return m;
+        long two = 1;
+        while(two <= n) two = two << 1;
+        if(two / 2 > m) return 0;
+        long mm = m;
+        long nn = n;
+        int result = mm++;
+        while(mm <= nn) result = result & mm++;
+        return result;
+    }
+};
