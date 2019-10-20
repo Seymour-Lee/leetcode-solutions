dfde8ee696da4c47cbb0cb619952c30fb02ef5af
Seymour-Lee <yli792@usc.edu>
2019-10-19 21:11:44 -0700

    add

--- sort/287. Find the Duplicate Number.cpp
+++ sort/287. Find the Duplicate Number.cpp
@@ -22,3 +22,12 @@ public:
         return 0;
     }
 };
+
+class Solution {
+public:
+    int findDuplicate(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        for(int i = 1; i < nums.size(); i++) if(nums[i-1] == nums[i]) return nums[i];
+        return -1;
+    }
+};
