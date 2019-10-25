548e348e79a8dea27736bc55d3f2e83272d71b85
Seymour-Lee <yli792@usc.edu>
2019-10-24 18:35:57 -0700

    add

--- math/029. Divide Two Integers.cpp
+++ math/029. Divide Two Integers.cpp
@@ -38,3 +38,15 @@ public:
         return sign*(int)res;
     }
 };
+
+class Solution {
+public:
+    int divide(int dividend, int divisor) {
+        int sign = (dividend<0 ^ divisor<0)? -1: 1;
+        long ldividend = labs(dividend);
+        long ldivisor = labs(divisor);
+        long res = exp(log(ldividend)-log(ldivisor));
+        if(res > INT_MAX) return sign == 1? INT_MAX: INT_MIN;
+        return sign*(int)res;
+    }
+};
