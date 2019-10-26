4d79e70ec5bdc6e32e173c7e9ff938034d82e64c
Seymour-Lee <yli792@usc.edu>
2019-10-25 19:15:42 -0700

    add

--- hashtable/442. Find All Duplicates in an Array.cpp
+++ hashtable/442. Find All Duplicates in an Array.cpp
@@ -14,3 +14,19 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    vector<int> findDuplicates(vector<int>& nums) {
+        vector<int> ans;
+        for(int i = 0; i < nums.size(); i++){
+            if(nums[abs(nums[i])-1] >= 0){
+                nums[abs(nums[i])-1] *= -1;
+            }
+            else{
+                ans.push_back(abs(nums[i]));
+            }
+        }
+        return ans;
+    }
+};
