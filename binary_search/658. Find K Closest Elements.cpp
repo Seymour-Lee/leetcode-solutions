ef8e3e13556513edfc25f411f590db9ba2df8fd7
Seymour-Lee <yli792@usc.edu>
2019-11-06 20:52:50 -0800

    add

--- binary_search/658. Find K Closest Elements.cpp
+++ binary_search/658. Find K Closest Elements.cpp
@@ -37,3 +37,29 @@ private:
         return l;
     }
 };
+
+class Solution {
+public:
+    vector<int> findClosestElements(vector<int>& arr, int k, int x) {
+        int l = binary(arr, x);
+        int r = l--;
+        vector<int> ans;
+        while(k--){
+            if(r >= arr.size() || l >= 0 && abs(x-arr[l]) <= abs(arr[r]-x)) ans.push_back(arr[l--]);
+            else ans.push_back(arr[r++]);
+        }
+        sort(ans.begin(), ans.end());
+        return ans;
+    }
+    
+private:
+    int binary(vector<int> &arr, int target){
+        int l = 0, r = arr.size();
+        while(l < r){
+            int m = l + (r - l) / 2;
+            if(arr[m] < target) l = m + 1;
+            else r = m;
+        }
+        return l;
+    }
+};
