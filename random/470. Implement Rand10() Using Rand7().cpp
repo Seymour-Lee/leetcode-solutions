12419fdfa99ababbb5fd2dd9334aee28be6008c0
Seymour-Lee <yli792@usc.edu>
2019-11-11 18:14:14 -0800

    add

--- random/470. Implement Rand10() Using Rand7().cpp
+++ random/470. Implement Rand10() Using Rand7().cpp
@@ -27,3 +27,16 @@ public:
         return 1+(ans-1) % 10;
     }
 };
+
+// The rand7() API is already defined for you.
+// int rand7();
+// @return a random integer in the range 1 to 7
+
+class Solution {
+public:
+    int rand10() {
+        int a = rand7();
+        int b = rand7();
+        return (a-1)*7+b <= 40? ((a-1)*7+b)%10+1: rand10();
+    }
+};
