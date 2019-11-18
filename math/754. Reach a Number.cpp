f3f73355cfa88f17f5e54d6d6c300642da641a69
Seymour-Lee <yli792@usc.edu>
2019-11-17 21:23:19 -0800

    add

--- math/754. Reach a Number.cpp
+++ math/754. Reach a Number.cpp
@@ -21,3 +21,13 @@ public:
         return target % 2 == 0? k: k + 1 + k % 2;
     }
 };
+
+class Solution {
+public:
+    int reachNumber(int target) {
+        target = abs(target);
+        int k = 0;
+        while(target > 0) target -= ++k;
+        return target % 2 == 0? k: k + 1 + k % 2;
+    }
+};
