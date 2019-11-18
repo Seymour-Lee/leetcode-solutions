217c03f20edf4b211f5ef314bfbb00290cc9c928
Seymour-Lee <yli792@usc.edu>
2019-11-18 14:30:31 -0800

    add

--- unclassfied/829. Consecutive Numbers Sum.cpp
+++ unclassfied/829. Consecutive Numbers Sum.cpp
@@ -8,3 +8,12 @@ public:
         return result;
     }
 };
+
+class Solution {
+public:
+    int consecutiveNumbersSum(int N) {
+        int ans = 1;
+        for(int k = 2; k < sqrt(2*N); k++) if((N - (k * (k - 1) / 2)) % k == 0) ans++;
+        return ans;
+    }
+};
