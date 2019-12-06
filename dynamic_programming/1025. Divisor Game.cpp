8da714eaedf143a3ca41e86c2275d83041a07183
Seymour-Lee <yli792@usc.edu>
2019-12-06 14:07:39 -0800

    add

--- dynamic_programming/1025. Divisor Game.cpp
+++ dynamic_programming/1025. Divisor Game.cpp
@@ -52,3 +52,20 @@ public:
     }
 };
 
+class Solution {
+public:
+    bool divisorGame(int N) {
+        vector<bool> dp(N+1, false);
+        for(int i = 1; i <= N; i++){
+            for(int j = 1; j < i; j++){
+                if(i % j) continue;
+                if(dp[i-j] == false){
+                    dp[i] = true;
+                    break;
+                }
+            }
+        }
+        return dp.back();
+    }
+};
+
