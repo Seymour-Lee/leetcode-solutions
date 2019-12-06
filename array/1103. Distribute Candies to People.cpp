7cfcbff4ad491b05d818d2826f0fecf993356b29
Seymour-Lee <yli792@usc.edu>
2019-12-05 18:06:14 -0800

    add

--- array/1103. Distribute Candies to People.cpp
+++ array/1103. Distribute Candies to People.cpp
@@ -13,3 +13,19 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<int> distributeCandies(int candies, int num_people) {
+        vector<int> ans(num_people, 0);
+        int counter = 1;
+        while(candies){
+            for(int i = 0; candies && i < num_people; i++, counter++){
+                int dis = min(counter, candies);
+                ans[i] += dis;
+                candies -= dis;
+            }
+        }
+        return ans;
+    }
+};
