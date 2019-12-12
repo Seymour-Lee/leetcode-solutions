d7e07797e76b025065ac9ea14713b7dded22b832
Seymour-Lee <yli792@usc.edu>
2019-12-12 12:57:37 -0800

    add

--- hashtable/202. Happy Number.cpp
+++ hashtable/202. Happy Number.cpp
@@ -32,3 +32,19 @@ public:
         return true;
     }
 };
+
+class Solution {
+public:
+    bool isHappy(int n) {
+        if(n <= 0) return false;
+        unordered_set<int> seen;
+        while(n != 1){
+            if(seen.find(n) != seen.end()) return false;
+            seen.insert(n);
+            int next = 0;
+            while(n) next += (n%10)*(n%10), n /= 10;
+            n = next;
+        }
+        return true;
+    }
+};
