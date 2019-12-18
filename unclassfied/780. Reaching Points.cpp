f17c14bb317db666735421068f676b317a61b1ef
Seymour-Lee <yli792@usc.edu>
2019-12-17 21:26:31 -0800

    add

--- unclassfied/780. Reaching Points.cpp
+++ unclassfied/780. Reaching Points.cpp
@@ -5,3 +5,22 @@ public:
         return sx == tx && (ty - sy) % sx == 0 || sy == ty && (tx - sx) % sy == 0;
     }
 };
+
+class Solution {
+public:
+    bool reachingPoints(int sx, int sy, int tx, int ty) {
+        while(tx >= sx && ty >= sy && tx != ty) {
+            if(tx > ty) {
+                if(tx == sx) return false;
+                if(ty == sy) return (tx - sx) % ty == 0;
+                tx %= ty;
+            }
+            else {
+                if(ty == sy) return false;
+                if(tx == sx) return (ty - sy) % tx == 0;
+                ty %= tx;
+            }
+        }
+        return (tx == sx && ty == sy);
+    }
+};
