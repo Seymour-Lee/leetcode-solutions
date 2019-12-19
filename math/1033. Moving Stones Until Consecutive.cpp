21eba38335cf7ef96dcd3eae487b488d32a0f82a
Seymour-Lee <yli792@usc.edu>
2019-12-18 23:33:00 -0800

    add

--- math/1033. Moving Stones Until Consecutive.cpp
+++ math/1033. Moving Stones Until Consecutive.cpp
@@ -73,3 +73,13 @@ public:
         return { min(s[1] - s[0], s[2] - s[1]) <= 2 ? 1 : 2, s[2] - s[0] - 2 };
     }
 };
+
+class Solution {
+public:
+    vector<int> numMovesStones(int a, int b, int c) {
+        vector<int> s = {a, b, c};
+        sort(s.begin(), s.end());
+        if (s[2] - s[0] == 2) return {0, 0};
+        return {min(s[1] - s[0], s[2] - s[1]) <= 2 ? 1 : 2, s[2] - s[0] - 2};
+    }
+};
