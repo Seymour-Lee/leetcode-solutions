8c7bbd3d113b0b6b99b252984147de7c95efe3e2
Seymour-Lee <yli792@usc.edu>
2019-12-19 01:04:39 -0800

    add

--- two_pointers/1040. Moving Stones Until Consecutive II.cpp
+++ two_pointers/1040. Moving Stones Until Consecutive II.cpp
@@ -14,3 +14,20 @@ public:
         return {low, high};
     }
 };
+
+class Solution {
+public:
+    vector<int> numMovesStonesII(vector<int>& A) {
+        sort(A.begin(), A.end());
+        int i = 0, n = A.size(), low = n;
+        int high = max(A[n - 1] - n + 2 - A[1], A[n - 2] - A[0] - n + 2);
+        for (int j = 0; j < n; ++j) {
+            while (A[j] - A[i] >= n) ++i;
+            if (j - i + 1 == n - 1 && A[j] - A[i] == n - 2)
+                low = min(low, 2);
+            else
+                low = min(low, n - (j - i + 1));
+        }
+        return {low, high};
+    }
+};
