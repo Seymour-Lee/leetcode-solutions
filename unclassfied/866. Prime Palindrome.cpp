50a9fecfc59006fe410444954dfaddb5184851dc
Seymour-Lee <yli792@usc.edu>
2019-12-20 21:50:00 -0800

    add

--- unclassfied/866. Prime Palindrome.cpp
+++ unclassfied/866. Prime Palindrome.cpp
@@ -28,3 +28,31 @@ private:
     }
     
 };
+
+class Solution {
+public:
+    int primePalindrome(int N) {
+        if(N <= 2) return 2;
+        if(N > 7 && N <= 11) return 11;
+        for(int i = 1; i < 999999; i++){
+            auto palin = reverse(i);
+            if(palin >= N && isPrime(palin)) return palin;
+        }
+        return -1;
+    }
+    
+    int reverse(int x){
+        auto s = to_string(x), t = s;
+        std::reverse(t.begin(), t.end());
+        s.pop_back();
+        return stoi(s+t);
+    }
+    
+    bool isPrime(int x){
+        if(x%2 == 0) return false;
+        int sq = int(sqrt(x)) + 1;
+        for(int i = 3; i<=sq; i+= 2)
+            if(x % i == 0) return false;
+        return true;
+    }
+};
