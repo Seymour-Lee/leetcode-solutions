f7a28d78752d9caeb046b5bf2995dcc2d0f487cd
Seymour-Lee <yli792@usc.edu>
2019-12-21 18:19:03 -0800

    add

--- math/883. Projection Area of 3D Shapes.cpp
+++ math/883. Projection Area of 3D Shapes.cpp
@@ -37,3 +37,19 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    int projectionArea(vector<vector<int>>& g) {
+        int ans = 0;
+        vector<int> r(g.size(), 0), c(g[0].size(), 0);
+        for(int i = 0; i < g.size(); i++) for(int j = 0; j < g[i].size(); j++){
+            ans += (g[i][j] > 0);
+            r[i] = max(r[i], g[i][j]);
+            c[j] = max(c[j], g[i][j]);
+        }
+        for(auto e: r) ans += e;
+        for(auto e: c) ans += e;
+        return ans;
+    }
+};
