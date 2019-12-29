053037759e984dc17d7fd9cdb46f336c29a95ca9
Seymour-Lee <yli792@usc.edu>
2019-12-28 17:45:30 -0800

    add

--- unclassfied/991. Broken Calculator.cpp
+++ unclassfied/991. Broken Calculator.cpp
@@ -10,3 +10,16 @@ public:
         return ans + X - Y;
     }
 };
+
+class Solution {
+public:
+    int brokenCalc(int X, int Y) {
+        int ans = 0;
+        while(Y > X){
+            ans++;
+            if(Y % 2 == 1) Y++;
+            else Y = Y / 2;
+        }
+        return ans + X - Y;
+    }
+};
