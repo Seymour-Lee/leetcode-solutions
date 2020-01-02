88c83d274d93cc1e593f7c6b36982c9d356ff8c2
Seymour-Lee <yli792@usc.edu>
2020-01-02 03:25:06 -0800

    add

--- string/949. Largest Time for Given Digits.cpp
+++ string/949. Largest Time for Given Digits.cpp
@@ -29,3 +29,18 @@ public:
         return ans.substr(0, 2) + ":" + ans.substr(2, 2);
     }
 };
+
+class Solution {
+public:
+    string largestTimeFromDigits(vector<int>& A) {
+        sort(A.rbegin(), A.rend());
+        string num;
+        for(auto a: A) num.push_back(a+'0');
+        while([](string str){
+                return str.substr(0, 2) <= "23" && str.substr(2, 2) <= "59";
+            }(num) == false){
+            if(prev_permutation(num.begin(), num.end()) == false) return "";
+        }
+        return num.substr(0, 2) + ":" + num.substr(2, 2);
+    }
+};
