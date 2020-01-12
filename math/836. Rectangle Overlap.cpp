5ff773ed73d5c24af8eed54942328a09faab3683
Seymour-Lee <yli792@usc.edu>
2020-01-11 17:13:27 -0800

    add

--- math/836. Rectangle Overlap.cpp
+++ math/836. Rectangle Overlap.cpp
@@ -17,3 +17,13 @@ public:
                  rec1[1] >= rec2[3]);
     }
 };
+
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& r1, vector<int>& r2) {
+        return r1[2] > r2[0] &&
+               r1[3] > r2[1] &&
+               r1[0] < r2[2] &&
+               r1[1] < r2[3];
+    }
+};
