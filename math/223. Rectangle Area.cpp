53c2a189b25b6a023f82f2283cbb571fb5d11965
Seymour-Lee <yli792@usc.edu>
2020-01-12 00:08:18 -0800

    add

--- math/223. Rectangle Area.cpp
+++ math/223. Rectangle Area.cpp
@@ -23,3 +23,16 @@ public:
         return total - (x[2] - x[1]) * (y[2] - y[1]);
     }
 };
+
+class Solution {
+public:
+    int computeArea(int A, int B, int C, int D, int E, int F, int G, int H) {
+        long long total = long(C-A) * (D-B) + long(G-E) * (H-F);
+        if(C<=E || G<=A || H<=B || D<=F) return total;
+        vector<long long> x{A, C, E, G};
+        vector<int> y{B, D, F, H};
+        sort(x.begin(), x.end());
+        sort(y.begin(), y.end());
+        return total - (x[2] - x[1]) * (y[2] - y[1]);
+    }
+};
