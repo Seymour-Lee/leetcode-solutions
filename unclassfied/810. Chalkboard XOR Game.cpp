fea5aa1170e816d11506b91d8a13d7afbc6eaf3b
Seymour-Lee <yli792@usc.edu>
2020-01-13 22:35:32 -0800

    add

--- unclassfied/810. Chalkboard XOR Game.cpp
+++ unclassfied/810. Chalkboard XOR Game.cpp
@@ -6,3 +6,12 @@ public:
         return x == 0 || nums.size() % 2 == 0;
     }
 };
+
+class Solution {
+public:
+    bool xorGame(vector<int>& nums) {
+        int x = 0;
+        for (int v : nums) x ^= v;
+        return x == 0 || nums.size() % 2 == 0;
+    }
+};
