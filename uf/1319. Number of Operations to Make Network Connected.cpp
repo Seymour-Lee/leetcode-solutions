e361db9e99df9533ac4acca82cd9c6b6f6b32d09
Seymour-Lee <yli792@usc.edu>
2020-01-20 12:48:48 -0800

    add

---
+++ uf/1319. Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        if(connections.size() < n-1) return -1;
+        p = vector<int>(n, -1);
+        for(int i = 0; i < connections.size(); i++){
+            u(connections[i][0], connections[i][1]);
+        }
+        return count(p.begin(), p.end(), -1)-1;
+    }
+        
+private:
+    vector<int> p;
+    
+    int f(int x){
+        if(p[x] == -1) return x;
+        return p[x] = f(p[x]);
+    }
+    
+    void u(int x, int y){
+        int px = f(x);
+        int py = f(y);
+        if(px != py) p[px] = py;
+    }
+};
