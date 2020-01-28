6366cceb2872c2cf8ab2fdd58e0cbf025e8c4d9c
Seymour-Lee <yli792@usc.edu>
2020-01-27 17:25:56 -0800

    add

--- array/047. Permutations II.cpp
+++ array/047. Permutations II.cpp
@@ -48,3 +48,13 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    vector<vector<int>> permuteUnique(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        vector<vector<int>> ans = {nums};
+        while(next_permutation(nums.begin(), nums.end())) ans.push_back(nums);
+        return ans;
+    }
+};
--- array/060. Permutation Sequence.cpp
+++ array/060. Permutation Sequence.cpp
@@ -39,3 +39,15 @@ public:
         return ans;
     }
 };
+
+class Solution {
+public:
+    string getPermutation(int n, int k) {
+        string ans = "";
+        vector<int> a;
+        for(int i = 1; i <= n; i++) a.push_back(i);
+        while(--k) next_permutation(a.begin(), a.end());
+        for(auto e: a) ans += char(e+'0');
+        return ans;
+    }
+};
