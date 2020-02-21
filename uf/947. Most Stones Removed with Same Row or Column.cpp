1170d346516353378ef0ecde30edfb7992a1f1e2
Seymour-Lee <yli792@usc.edu>
2020-02-20 23:11:21 -0800

    add

--- uf/947. Most Stones Removed with Same Row or Column.cpp
+++ uf/947. Most Stones Removed with Same Row or Column.cpp
@@ -50,3 +50,26 @@ private:
         if(px != py) p[px] = py;
     }
 };
+
+class Solution {
+public:
+    int removeStones(vector<vector<int>>& stones) {
+        p = vector<int>(stones.size(), -1);
+        for(int i = 0; i < stones.size(); i++) for(int j = i+1; j < stones.size(); j++) 
+            if(stones[i][0] == stones[j][0] || stones[i][1] == stones[j][1]) u(i, j);
+        return stones.size() - count(p.begin(), p.end(), -1);
+    }
+    
+private:
+    vector<int> p;
+        
+    int f(int x){
+        if(p[x] == -1) return x;
+        return p[x] = f(p[x]);
+    }
+    
+    void u(int x, int y){
+        int px = f(x), py = f(y);
+        if(px != py) p[px] = py;
+    }
+};
