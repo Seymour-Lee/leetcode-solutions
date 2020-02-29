5051b720c4dc2fec6bb4de0882c9461b35eb5617
Seymour-Lee <yli792@usc.edu>
2020-02-29 00:02:05 -0800

    add

--- unclassfied/474. Ones and Zeroes.cpp
+++ unclassfied/474. Ones and Zeroes.cpp
@@ -12,3 +12,18 @@ public:
         return dp.back().back();
     }
 };
+
+class Solution {
+public:
+    int findMaxForm(vector<string>& strs, int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1, 0));
+        for(auto s: strs){
+            int ones = count(s.begin(), s.end(), '1');
+            int zeros = s.size() - ones;
+            for(int i = m; i >= zeros; i--)
+                for(int j = n; j >= ones; j--)
+                    dp[i][j] = max(dp[i][j], dp[i-zeros][j-ones]+1);
+        }
+        return dp.back().back();
+    }
+};
