946a3a7635fc1a81c138fd96eba4d5c668cebf98
Seymour-Lee <yli792@usc.edu>
2020-02-28 21:22:08 -0800

    add

--- unclassfied/576. Out of Boundary Paths.cpp
+++ unclassfied/576. Out of Boundary Paths.cpp
@@ -32,3 +32,19 @@ public:
 private:
     vector<vector<vector<int>>> dp = vector<vector<vector<int>>>(51, vector<vector<int>>(51, vector<int>(51, -1)));
 };
+
+class Solution {
+public:
+    int findPaths(int m, int n, int N, int i, int j) {
+        int mod = 1e9 + 7;
+        if(i < 0 || j < 0 || i >= m || j >= n) return 1;
+        if(N == 0) return 0;
+        if(dp[i][j][N] >= 0) return dp[i][j][N];
+        dp[i][j][N] = ((findPaths(m, n, N-1, i-1, j) + findPaths(m, n, N-1, i+1, j)) % mod 
+                    + (findPaths(m, n, N-1, i, j+1) + findPaths(m, n, N-1, i, j-1)) % mod) % mod;
+        return dp[i][j][N];
+    }
+    
+private:
+    vector<vector<vector<int>>> dp = vector<vector<vector<int>>>(51, vector<vector<int>>(51, vector<int>(51, -1)));
+};
