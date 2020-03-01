363a31a6988dd797ed90bf0113237d988ef1baa9
Seymour-Lee <yli792@usc.edu>
2020-03-01 00:06:22 -0800

    add

--- unclassfied/837. New 21 Game.cpp
+++ unclassfied/837. New 21 Game.cpp
@@ -11,3 +11,17 @@ public:
         return dp[0];
     }
 };
+
+class Solution {
+public:
+    double new21Game(int N, int K, int W) {
+        vector<double> dp(N+W+1, 0.0);
+        for(int k = K; k <= N; k++) dp[k] = 1.0;
+        double s = min(N-K+1, W);
+        for(int k = K-1 ; k >= 0; k--){
+            dp[k] = s / W;
+            s += dp[k] - dp[k+W];
+        }
+        return dp[0];
+    }
+};
