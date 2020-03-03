532d63915d5eba20655ef4e7231a3938845d2f17
Seymour-Lee <yli792@usc.edu>
2020-03-02 19:31:14 -0800

    add

--- dynamic_programming/139. Word Break.cpp
+++ dynamic_programming/139. Word Break.cpp
@@ -62,3 +62,21 @@ public:
         return dp.back();
     }
 };
+
+class Solution {
+public:
+    bool wordBreak(string s, vector<string>& wordDict) {
+        unordered_set<string> strs(wordDict.begin(), wordDict.end());
+        vector<bool> dp(s.size()+1, false);
+        dp[0] = true;
+        for(int j = 1; j <= s.size(); j++){
+            for(int i = 0; i < j; i++){
+                if(dp[i] && strs.find(s.substr(i, j-i)) != strs.end()){
+                    dp[j] = true;
+                    break;
+                }
+            }
+        } 
+        return dp.back();
+    }
+};
