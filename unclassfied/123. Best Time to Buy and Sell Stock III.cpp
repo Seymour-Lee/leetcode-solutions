001e9ae84243620d2f56f13ae836a093ccc2a268
Seymour-Lee <yli792@usc.edu>
2020-03-12 22:47:55 -0700

    add

--- unclassfied/123. Best Time to Buy and Sell Stock III.cpp
+++ unclassfied/123. Best Time to Buy and Sell Stock III.cpp
@@ -12,3 +12,18 @@ public:
         return max(0, max(sell1, sell2));
     }
 };
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int buy1 = INT_MIN, buy2 = INT_MIN;
+        int sell1 = 0, sell2 = 0;
+        for(auto p: prices){
+            buy1 = max(buy1, -p);
+            sell1 = max(sell1, buy1+p);
+            buy2 = max(buy2, sell1-p);
+            sell2 = max(sell2, buy2+p);
+        }
+        return max(0, max(sell1, sell2));
+    }
+};
